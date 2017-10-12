ad6fd329add274e13e4098df293b28b2fcddc343
Nick Casciola <s0948644@monmouth.edu>
2017-10-12 18:41:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ function happyHolidays() {
 happyHolidaysTo('you');
 
 function happyHolidaysTo(name) {
-  return 'Happy Holidays, ${name}!';
+  return `Happy holidays, ${name}!`;
 }
 
 happyHolidayTo('Independence Day', 'you');
 
 function happyHolidayTo(name, holiday) {
-  return 'Happy ${holiday}, ${name}!';
+  return `Happy ${holiday}, ${name}!`;
 }
 
 holidayCountdown(20, 'Mother\'s Day')
 
 function holidayCountdown(days, holiday) {
-  return 'It\'s ${days} days until ${holiday}!';
+  return `It\'s ${days} days until ${holiday}!`;
 }
